e30fdc4f692778c01630044a9b12ec7cd65f7e88
Marx-A00 <mnandrade1999@gmail.com>
2026-08-08 08:09:54 -0500

    acp: pin acp-mobile c3f3afc (peek sheet)

--- macos/acp/build-acp-tools.sh
+++ macos/acp/build-acp-tools.sh
@@ -15,7 +15,7 @@ ACP_MULTIPLEX_COMMIT="d987060"
 # + labels.json sidecar, iOS standalone polish)
 # + local 714ab25 (rig-match: Iosevka Term Slab webfont, org-level
 # grayscale headers, purple links)
-ACP_MOBILE_COMMIT="4f9fd67"
+ACP_MOBILE_COMMIT="c3f3afc"
 
 mkdir -p "$SRC_DIR" "$BIN_DIR"
 
