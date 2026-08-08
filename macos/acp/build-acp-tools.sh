41042b27e81702a680eb2f4dd8d310d2d9001717
Marx-A00 <mnandrade1999@gmail.com>
2026-08-08 12:30:29 -0500

    acp: pin acp-mobile 8b276b1 (send feel v3)

--- macos/acp/build-acp-tools.sh
+++ macos/acp/build-acp-tools.sh
@@ -15,7 +15,7 @@ ACP_MULTIPLEX_COMMIT="d987060"
 # + labels.json sidecar, iOS standalone polish)
 # + local 714ab25 (rig-match: Iosevka Term Slab webfont, org-level
 # grayscale headers, purple links)
-ACP_MOBILE_COMMIT="5374335"
+ACP_MOBILE_COMMIT="8b276b1"
 
 mkdir -p "$SRC_DIR" "$BIN_DIR"
 
