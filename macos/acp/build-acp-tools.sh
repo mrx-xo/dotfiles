217169da4b1008f48084ae512471327490f6c656
Marx-A00 <mnandrade1999@gmail.com>
2026-08-09 18:25:47 -0500

    acp: pin acp-mobile 79924d0 (reader text-size fix)

--- macos/acp/build-acp-tools.sh
+++ macos/acp/build-acp-tools.sh
@@ -15,7 +15,7 @@ ACP_MULTIPLEX_COMMIT="d987060"
 # + labels.json sidecar, iOS standalone polish)
 # + local 714ab25 (rig-match: Iosevka Term Slab webfont, org-level
 # grayscale headers, purple links)
-ACP_MOBILE_COMMIT="69b65a5"
+ACP_MOBILE_COMMIT="79924d0"
 
 mkdir -p "$SRC_DIR" "$BIN_DIR"
 
