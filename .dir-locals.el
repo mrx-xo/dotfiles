7a0314c638767625200023d965e34ad77fc9607e
Marx-A00 <mnandrade1999@gmail.com>
2026-09-01 14:44:46 -0500

    emacs: fix project command dir-local variable

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables for the dotfiles repo  -*- no-byte-compile: t -*-
 ;;; Per-project named commands, run via SPC p RET (mr-x/project-command).
 
-((nil . ((mr-x/project-cmd-alist
+((nil . ((mr-x/project-commands
           . (("push"   . "macos/scripts/lights/lightsctl push")
              ("deploy" . "macos/scripts/lights/lightsctl deploy")
              ("test"   . "macos/scripts/lights/lightsctl status"))))))
